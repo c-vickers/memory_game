29b6178a27e8e70accd210c22c9d471a7bebb793
vickc008 <christina.vickers@disney.com>
2018-01-30 13:47:11 -0800

    Unit 9

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,19 +2,22 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-console.log("User flipped " + cardOne);
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardTwo);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2 ){
-  if (cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match");
-  } else {
-    alert("Sorry, try again");
+function checkForMatch(){
+  if (cardsInPlay.length === 2 ){
+    if (cardsInPlay[0] === cardsInPlay[1]){
+      alert("You found a match");
+    } else {
+      alert("Sorry, try again");
+    }
   }
 }
+
+function flipCard (cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
